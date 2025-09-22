0db9724d093b5267882a6d2001aa00af452a37a2
nagib-1 <nagib_akbari@hotmail.de>
2025-09-22 22:31:48 +0200

    expand script

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=200&offset=0";
+let allPokemons = [];
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0";
 
-function init() {
 
+async function init() {
+    let response = await fetch(BASE_URL)
+    let keysArray = await response.json();
+    for (let index = 0; index < keysArray.length; index++) {
+        allPokemons.push({
+            id: keysArray[index],
+            pokemon: response[keysArray[index]]
+        })
+
+    }
+    console.log(keysArray.results[0]);
+    document.getElementById('content'). innerHTML = keysArray.results[0];
 }
