cf62f77e5d1d9c02ea0fc17aab938deb9b6d560c
nagib-1 <nagib_akbari@hotmail.de>
2025-10-01 13:50:55 +0200

    added load more button

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const allPokemons = [];
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
-// "https://pokeapi.co/api/v2/pokemon?limit=40&offset=21"; nächste 20
 const TYPE_ICON = {
   bug: "/icons/bug.svg",
   dark: "/icons/dark.svg",
@@ -21,7 +19,8 @@ const TYPE_ICON = {
   steel: "/icons/steel.svg",
   water: "/icons/water.svg",
 };
-
+const limit = 20;
+let offset = 20;
 
 async function init() {
   const response = await fetch(BASE_URL)
@@ -60,10 +59,45 @@ async function init() {
   }
 }
 
-function loadMore() {
-  const nextTwenty = 20;
-  let nextURL = `https://pokeapi.co/api/v2/pokemon?limit=${nextTwenty}&offset=0`;
-  init();
+async function loadMore() {
+  const nextUrl = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+  const response = await fetch(nextUrl)
+  const data = await response.json();
+
+  for (let index = 0; index < data.results.length; index++) {
+    const names = data.results[index];
+    const detailRes = await fetch(names.url);
+    const detailData = await detailRes.json();
+    const imgUrl = detailData.sprites.other.dream_world.front_default;
+    const firstAttribut = detailData.types[0].type.name;
+    const firstAttributIcon = TYPE_ICON[firstAttribut];
+    const secondAttribut = detailData.types[1]?.type.name;
+    const secondAttributIcon = secondAttribut ? TYPE_ICON[secondAttribut] : null;
+
+    let icons = `<img class="attribut-icon" src="${firstAttributIcon}">`;
+    if (secondAttributIcon) {
+      icons += `<img class="attribut-icon" src="${secondAttributIcon}">`;
+    }
+
+    const card = `
+    <div class="card-cover">
+      <div class="card-header">
+        <h5>#${index + offset + 1}</h5>
+        <h4>${names.name}</h4>
+      </div>
+      <div class="pokemon-image">
+        <img src="${imgUrl}">
+      </div>
+      <div class="attributes">
+        ${icons}
+      </div>
+    </div>
+    `
+    document.getElementById('content').innerHTML += card;
+  }
+
+  offset += limit;
+  index += 20;
 }
 
 function showDetails() {
